2fe88f6caf1b3b47cff176c584d1002970aedcdd
Alverna <alvernaleo036@gmail.com>
2025-10-28 15:34:37 +0530

    ii
    kk

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using employe.appmodel.models;
+
+namespace employe.appmodel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//            Create an abstract class Employee with:
+//o Private properties: EmployeeId, Name, BasicSalary
+//o Constructor to initialize them
+//o Public methods:
+// GetEmployeeId(), GetName(), GetBasicSalary()
+// SetBasicSalary(double amount) to update salary with validations
+// An abstract method CalculateSalary() to be implemented by child
+//classes
+
+            var emp =new FullTimeEmploye(...);
+
+            Console.WriteLine( emp.calculatesalary() );
+        }
+    }
+}
