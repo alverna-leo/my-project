172e18a2cf6022f057aab8993179a108f27b6c09
alverna-leo <alvernaleo036@gmail.com>
2026-02-08 20:15:24 +0530

    Remove old projects and start fresh

--- StyleBay-Project/StyleBay_Version2/StyleBay_Version2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StyleBay_Version2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Workshop/JobProviderApp/Dto/JobProviderDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobProviderApp.Dto
-{
-    public class JobProviderDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
